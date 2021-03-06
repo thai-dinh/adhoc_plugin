7ab4fbf6a9d0406f937dc7b689266f8d6d59b6fe
Thai Dinh Le <Thai-Dinh@hotmail.com>
2021-03-06 12:53:26 +0100

    Update Ad Hoc App.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:adhoclibrary/adhoclibrary.dart';
 import 'package:adhoclibrary_example/distributed_cache/music_search_engine.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(AdHocMusicClient());
@@ -13,7 +14,7 @@ class _AdHocMusicClientState extends State<AdHocMusicClient> {
   TransferManager _manager = TransferManager(true);
   List<AdHocDevice> _devices = List.empty(growable: true);
 
-/*------------------------------Override methods------------------------------*/
+/*-----------------------------Override methods------------------------------*/
 
   @override
   void initState() {
@@ -41,51 +42,67 @@ class _AdHocMusicClientState extends State<AdHocMusicClient> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: <String, WidgetBuilder> {
-        '/search' : (BuildContext context) => MusicSearchEngine(),
+        '/search' : (BuildContext context) => MusicSearchEngine(['Test']),
       },
-      home: Scaffold(
-        appBar: AppBar(title: Center(child: Text('Ad Hoc Music Client'))),
-        body: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Expanded(
-              child: Column(
-                children: <Widget>[
-                  ElevatedButton(
-                    child: Center(child: Text('Search for music')),
-                    onPressed: () => Navigator.of(context).pushNamed('/search'),
-                  ),
-                  ElevatedButton(
-                    child: Center(child: Text('Search for nearby devices')),
-                    onPressed: () => _manager.discovery(),
-                  ),
-                  Expanded(
-                    child: ListView(
-                      children: _devices.map((device) {
-                        return Card(
-                          child: ListTile(
-                            title: Center(child: Text(device.name)),
-                            subtitle: Center(child: Text(device.mac)),
-                            onTap: () {
-                              _manager.connect(device);
-                              setState(() {
-                                _devices.removeWhere(
-                                  (element) => (element.mac == device.mac)
-                                );
-                              });
-                            },
-                          ),
-                        );
-                      }).toList(),
+      home: Builder(
+        builder: (context) => Scaffold(
+          appBar: AppBar(title: Center(child: Text('Ad Hoc Music Client'))),
+          body: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    ElevatedButton(
+                      child: Center(child: Text('Search for music')),
+                      onPressed: () => Navigator.pushNamed(context, '/search'),
                     ),
-                  ),
-                ],
+                    ElevatedButton(
+                      child: Center(child: Text('Search for nearby devices')),
+                      onPressed: () => _manager.discovery(),
+                    ),
+                    ElevatedButton(
+                      child: Center(child: Text('Search for nearby devices')),
+                      onPressed: () {
+                        AssetsAudioPlayer.newPlayer().open(
+                          Audio("assets/audios/song1.mp3"),
+                          autoStart: true,
+                          showNotification: true,
+                        );
+                      },
+                    ),
+                    Expanded(
+                      child: ListView(
+                        children: _devices.map((device) {
+                          return Card(
+                            child: ListTile(
+                              title: Center(child: Text(device.name)),
+                              subtitle: Center(child: Text(device.mac)),
+                              onTap: () {
+                                _manager.connect(device);
+                                setState(() {
+                                  _devices.removeWhere(
+                                    (element) => (element.mac == device.mac)
+                                  );
+                                });
+                              },
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
+
+/*------------------------------Private methods------------------------------*/
+
 }
